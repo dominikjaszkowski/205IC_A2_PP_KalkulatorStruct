fff9e5e4b7f93dc5b262d3e4fedb0f9b9d05f150
Dominik <ja-do@wp.pl>
2021-01-21 15:06:41 +0100

    main

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "kalkulator.h"
+#include "stworz.h"
+#include "wykonaj.h"
 
 int main() {
     struct dzialanie *mojedzialanie = wczytajdzialanie();
